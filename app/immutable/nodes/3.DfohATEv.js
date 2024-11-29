c8142b4adc048498e39cafb6f17b1bdb68856a54
Alzaroth <allan.ahu@gmail.com>
2024-11-29 21:52:53 +0900

    Updates

--- app/immutable/nodes/3.DfohATEv.js
+++
@@ -1,1 +0,0 @@
-import{a,t as p}from"../chunks/disclose-version.DfTYQCr2.js";import"../chunks/title.CiAU7VvV.js";var r=p("<h2>About</h2>");function n(o){var t=r();a(o,t)}export{n as component};
---
+++ app/immutable/nodes/4.Cb3QL_A5.js
@@ -0,0 +1,1 @@
+import{a as o,t as p}from"../chunks/disclose-version.DfTYQCr2.js";import"../chunks/title.B5iXJVw6.js";var r=p("<h2>Contact</h2>");function m(t){var a=r();o(t,a)}export{m as component};
